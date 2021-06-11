d3c08c5cc873f9f3f2da6457925ce1f0bc752636
Ryan <Harmon>
2021-06-11 13:16:43 -0600

    Disable seed

--- database/seeders/DatabaseSeeder.php
+++ database/seeders/DatabaseSeeder.php
@@ -15,8 +15,8 @@ class DatabaseSeeder extends Seeder
      */
     public function run()
     {
-        Fragment::factory()
-            ->count(50)
-            ->create();
+        // Fragment::factory()
+        //     ->count(50)
+        //     ->create();
     }
 }
